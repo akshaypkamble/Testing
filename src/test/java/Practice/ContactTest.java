52b149b934001ea21ff9a3caaeef11b6922b6130
HP <akshaypkamble393@gmail.com>
2022-09-20 18:25:22 +0530

    First Commit - Engineer 1

---
+++ src/test/java/Practice/ContactTest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void contact()
+	{
+		System.out.println("This is my first commit - MavenProjects");
+	}
+
+}
